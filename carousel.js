bebdf714b0d97df591679b1110c0a44bdc0f1d1f
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-02-07 16:55:25 +0100

    Controls via carousel nav added.

--- carousel.js
+++ carousel.js
@@ -1,17 +1,13 @@
-const track = document.querySelector(".product-carousel__track") // <ul> element
+const track = document.querySelector(".product-carousel__track"); // <ul> element
 const slides = Array.from(track.children);
-const nextButton = document.querySelector("#carousel-next-button")
-const prevButton = document.querySelector("#carousel-prev-button")
-const carouselNav = document.querySelector(".carousel-navigation")
-const carouselNavItems = Array.from(carouselNav.children)
+const nextButton = document.querySelector("#carousel-next-button");
+const prevButton = document.querySelector("#carousel-prev-button");
+const carouselNav = document.querySelector(".carousel-navigation");
+const carouselNavItems = Array.from(carouselNav.children);
+const navItemsMap = new Map();
 
 const slideWidth= slides[0].getBoundingClientRect().width; 
 
-
-// arrange the slides next to one another
-const setSlidePosition = (slide, index) => {
-    slide.style.left = `${slideWidth * index}px`;
-}
 
 const moveToSlide = (track, currentSlide, targetSlide) => {
     // If there is no slide to move to 
@@ -22,8 +18,14 @@ const moveToSlide = (track, currentSlide, targetSlide) => {
     track.style.transform = `translateX(-${targetSlide.style.left})`;
     currentSlide.removeAttribute("data-current-slide");
     targetSlide.setAttribute("data-current-slide", "")
-    
 }
+
+
+// Arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+    slide.style.left = `${slideWidth * index}px`;
+}
+
 slides.forEach(setSlidePosition);
 
 
@@ -45,20 +47,33 @@ nextButton.addEventListener("click", e => {
 })
 
 
-// Toggle between carousel navigation items
-const toggleCarouselNavItems = e => {
-    console.log(item)
+// Controlling the carousel with navigation
+
+// Bind each carousel nav item to a number from 1, 4.
+const linkNavItems = (item, index) => {
+    navItemsMap.set(item, (index + 1).toString())
 }
+
+carouselNavItems.forEach(linkNavItems)
 
 
 carouselNav.addEventListener("click", e => {
     const selectedItem = e.target.closest(".carousel-navigation__item");
-
+    
     if (!selectedItem) {
         return;
     }
-
+    
     for (let carouselNavItem of carouselNavItems) {
         carouselNavItem.classList.toggle("carousel-navigation__item--selected", carouselNavItem === selectedItem)
     }
+    
+    // Use selected item as a key in the map and get slide number
+    const slideNumber = navItemsMap.get(selectedItem);
+
+    // Using the query selector we select a carousel slide with a data-slide value matching the number we got from the map
+    const currentSlide = track.querySelector("[data-current-slide]");
+    const targetSlide = track.querySelector(`.product-carousel__slide[data-slide='${slideNumber}']`);
+
+    moveToSlide(track, currentSlide, targetSlide);
 })
